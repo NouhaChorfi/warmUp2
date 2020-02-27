8b746fee49b21496a4c8d0d7601ae4293fcc476d
NouhaChorfi <nouhachorfi2@gmail.com>
2020-02-27 10:09:36 +0100

    warmup19 Nouha chorfi

--- warmUp19/warmUp-19.js
+++ warmUp19/warmUp-19.js
@@ -1,7 +1,8 @@
 /*
 Say you have an array for which the i element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.
 
@@ -19,5 +20,38 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
 var maxProfit = function(prices) {
-  // YOUR CODE HERE
+  var prices=prices;
+  var sell=sell;
+  var transaction=0;
+  var max=0;
+  var profit=0;
+  
+
+  return{
+     buy:function(price){
+        if ( transaction%2 === 0 ){
+          prices.push(price)
+          transaction+=1
+        }
+     },
+     
+     sell:function(sellprice){
+      if ( transaction % 2 !== 0 && sellprice !== 0 ){
+         for (var i=0; i < prices.length; i++){
+           if ( max <= prices[i]){
+             max = prices[i]
+             sell = sellprice;
+           }
+         }
+        }
+     },
+     profits:function(){
+       if(max !== 0){
+         profit = max-sell;
+       }
+
+     }
+
+    }
+ 
 };
